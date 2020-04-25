2dd5fcdb47d5aa7764e8e58907a765c56029d50f
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-04-25 12:16:54 -0400

    added sideDrawer animation

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -8,22 +8,29 @@ import Background from 'components/ui/Background/Background'
 import NavigationItem from '../NavigationsItems/NavigationItem/NavigationItem'
 import trending from 'assets/trending.png'
 import web from 'assets/web.png'
+import { motion, AnimatePresence } from 'framer-motion'
 
 function SideDrawer({ show, setShow }) {
-    let sideDrawer = (
-        <div className={classes.SideDrawer} onClick={() => setShow(false)}>
-            <Background src={background} />
-            <Logo src={logo} width="15%" height="auto" />
-            <NavigationItems>
-                <NavigationItem src={trending} to="/" exact>Trending</NavigationItem>
-                <NavigationItem src={web} to="/web-dev">Web development</NavigationItem>
-            </NavigationItems>
-        </div>
-    )
-
-    if (!show) sideDrawer = null;
-
-    return sideDrawer;
+    return (
+        <AnimatePresence>
+            {show && (
+                <motion.div
+                    initial={{ x: -100 }}
+                    animate={{ x: 0 }}
+                    transition={{ duration: .3 }}
+                    exit={{ x: -250 }}
+                    className={classes.SideDrawer}
+                    onClick={() => setShow(false)}>
+                    <Background src={background} />
+                    <Logo src={logo} width="15%" height="auto" />
+                    <NavigationItems>
+                        <NavigationItem src={trending} to="/" exact>Trending</NavigationItem>
+                        <NavigationItem src={web} to="/web-dev">Web development</NavigationItem>
+                    </NavigationItems>
+                </motion.div>
+            )}
+        </AnimatePresence>
+    );
 }
 
 export default SideDrawer
