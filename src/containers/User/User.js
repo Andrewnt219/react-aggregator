875b6ef03265b105a1a0bd5d51b999b143e55ae6
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-05-04 16:14:10 -0400

    added loading to profile page

--- src/containers/User/User.js
+++ src/containers/User/User.js
@@ -14,6 +14,7 @@ import { storeUserResponse, selectIsLoading, getUserReponses, selectUserResponse
 import Response from 'components/User/Response/Response'
 import Popup from 'components/ui/Popup/Popup';
 import AvatarSelection from 'components/User/AvatarSelection/AvatarSelection'
+import Spinner from 'components/ui/Spinner/Spinner';
 
 function User() {
     const dispatch = useDispatch();
@@ -37,69 +38,73 @@ function User() {
         dispatch(storeUserResponse({ ...data, isResolved: false, resolvedMessage: "", issuedDate: new Date() }));
     }
 
-    return (
-        <div className={classes.User}>
-            <Card >
-                <Avatar
-                    setShowAvatarSelection={setShowAvatarSelection}
-                    src={user.avatar ? user.avatar : avatars[0]} />
-                <h1>{user.displayName}</h1>
-            </Card>
+    let profile = <Spinner />
+    if (!isLoading) {
+        profile = (
+            <div className={classes.User}>
+                <Card >
+                    <Avatar
+                        setShowAvatarSelection={setShowAvatarSelection}
+                        src={user.avatar ? user.avatar : avatars[0]} />
+                    <h1>{user.displayName}</h1>
+                </Card>
 
-            <Popup
-                show={showAvatarSelection}
-                setShow={setShowAvatarSelection}
-                setNeedRefreshed={setNeedRefreshed}  >
-                <AvatarSelection
-                    setShowAvatarSelection={setShowAvatarSelection}
-                    id={user.id} />
-            </Popup>
+                <Popup
+                    show={showAvatarSelection}
+                    setShow={setShowAvatarSelection}
+                    setNeedRefreshed={setNeedRefreshed}  >
+                    <AvatarSelection
+                        setShowAvatarSelection={setShowAvatarSelection}
+                        id={user.id} />
+                </Popup>
 
-            <DropDownSetting icon={faInfoCircle} title="My profile" >
-                <p>Email: {user.email} </p>
-                <p>First name: {user.firstName}</p>
-                <p>Last name: {user.lastName}</p>
-                <p>Display name: {user.displayName}</p>
-            </DropDownSetting>
+                <DropDownSetting icon={faInfoCircle} title="My profile" >
+                    <p>Email: {user.email} </p>
+                    <p>First name: {user.firstName}</p>
+                    <p>Last name: {user.lastName}</p>
+                    <p>Display name: {user.displayName}</p>
+                </DropDownSetting>
 
-            <DropDownSetting icon={faInbox} title="Your messages">
-                {userResponses.length === 0
-                    ? <p>Your inbox is empty</p>
-                    : userResponses.map((response, idx) => <Response
-                        key={idx}
+                <DropDownSetting icon={faInbox} title="Your messages">
+                    {userResponses.length === 0
+                        ? <p>Your inbox is empty</p>
+                        : userResponses.map((response, idx) => <Response
+                            key={idx}
 
-                        subject={response.subject}
-                        message={response.message}
-                        isResolved={response.isResolved}
-                        issuedDate={response.issuedDate}
-                        resolvedMessage={response.resolvedMessage}
-                        id={response.id}
+                            subject={response.subject}
+                            message={response.message}
+                            isResolved={response.isResolved}
+                            issuedDate={response.issuedDate}
+                            resolvedMessage={response.resolvedMessage}
+                            id={response.id}
 
-                        setShowPopupMessage={setShowPopupMessage}
-                        setNeedRefreshed={setNeedRefreshed}
-                    />)}
-            </DropDownSetting>
-            <Popup
-                show={showPopupMessage}
-                setShow={setShowPopupMessage}
-                setNeedRefreshed={setNeedRefreshed}>
-                <p>Your message to the developer has been revoked</p>
-            </Popup>
+                            setShowPopupMessage={setShowPopupMessage}
+                            setNeedRefreshed={setNeedRefreshed}
+                        />)}
+                </DropDownSetting>
+                <Popup
+                    show={showPopupMessage}
+                    setShow={setShowPopupMessage}
+                    setNeedRefreshed={setNeedRefreshed}>
+                    <p>Your message to the developer has been revoked</p>
+                </Popup>
 
-            <PopUpSetting
-                setNeedRefreshed={setNeedRefreshed}
-                icon={faComment}
-                title="Contact us">
-                <Contact
-                    email={user.email}
-                    onSubmit={onSubmit}
-                    isLoading={isLoading} />
-            </PopUpSetting>
+                <PopUpSetting
+                    setNeedRefreshed={setNeedRefreshed}
+                    icon={faComment}
+                    title="Contact us">
+                    <Contact
+                        email={user.email}
+                        onSubmit={onSubmit}
+                        isLoading={isLoading} />
+                </PopUpSetting>
 
-            <Button clicked={() => dispatch(logout())}>LOG OUT</Button>
-        </div>
+                <Button clicked={() => dispatch(logout())}>LOG OUT</Button>
+            </div>
+        )
+    }
 
-    )
+    return profile;
 }
 
 export default User
