6201d8c1b4ced989e749e99de6879a75a87b0744
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-05-28 07:31:19 -0400

    fixed cors error

--- src/Axios.js
+++ src/Axios.js
@@ -1,5 +1,12 @@
-import axios from 'axios'
+import axios from 'axios';
 const instance = axios.create({
-    baseURL: 'https://react-aggregator.firebaseio.com'
-})
-export default instance
+  baseURL: 'https://react-aggregator.firebaseio.com',
+});
+instance.interceptors.response.use(
+  (response) => {
+    response.headers['Access-Control-Allow-Origin'] = '*';
+    return response;
+  },
+  (error) => Promise.reject(error)
+);
+export default instance;
