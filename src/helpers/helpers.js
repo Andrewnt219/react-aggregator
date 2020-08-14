b3f597a79ad43eca2f0ee700f465c8863e1c3448
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-08-14 11:33:01 -0400

    updated website status

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,4 +1,4 @@
-import { setError } from 'features/uiSlice';
+import { setError } from "features/uiSlice";
 
 export const objectToArrayObject = (object) =>
   Object.entries(object).map(([id, value]) => {
@@ -29,43 +29,51 @@ export const asyncDispatchWrapper = async function (
     // damn, no one follows a specific structure for error object
     let errorMessage;
     try {
-      errorMessage = error.response.data.error.message;
+      errorMessage = error.response
+        ? error.response.data.error.message
+        : "This website is down because NewsApi no longer provides access for production-ready sites. Check README for demo";
     } catch (err) {
       errorMessage = error.message;
     }
     switch (errorMessage.toUpperCase()) {
-      case 'NETWORK ERROR':
-        errorMessage = 'Check your connection';
+      case "NETWORK ERROR":
+        errorMessage = "Check your connection";
         break;
-      case 'EMAIL_EXISTS':
-        errorMessage = 'Email exists! Try signing in';
+      case "EMAIL_EXISTS":
+        errorMessage = "Email exists! Try signing in";
         break;
-      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-        errorMessage = 'Too manu failed attempts, please try again later';
+      case "TOO_MANY_ATTEMPTS_TRY_LATER":
+        errorMessage = "Too manu failed attempts, please try again later";
         break;
-      case 'EMAIL_NOT_FOUND':
-        errorMessage = 'Email not found! Try signing up';
+      case "EMAIL_NOT_FOUND":
+        errorMessage = "Email not found! Try signing up";
         break;
-      case 'INVALID_PASSWORD':
-        errorMessage = 'Invalid email or password';
+      case "INVALID_PASSWORD":
+        errorMessage = "Invalid email or password";
         break;
-      case 'USER_DISABLED':
-        errorMessage = 'Your account has been suspensed';
+      case "USER_DISABLED":
+        errorMessage = "Your account has been suspensed";
         break;
-      case 'WEAK_PASSWORD':
-        errorMessage = 'Password must be at least 6 characters';
+      case "WEAK_PASSWORD":
+        errorMessage = "Password must be at least 6 characters";
         break;
-      case 'INVALID_EMAIL':
-        errorMessage = 'Invalid email or password';
+      case "INVALID_EMAIL":
+        errorMessage = "Invalid email or password";
         break;
-      case 'MISSING_PASSWORD':
-        errorMessage = 'Invalid email or password';
+      case "MISSING_PASSWORD":
+        errorMessage = "Invalid email or password";
         break;
 
       default:
+        errorMessage =
+          "This website is down because NewsApi no longer provides access for production-ready sites. Check README for demo";
         break;
     }
-    dispatch(setError({ hasError: errorMessage }));
+    dispatch(
+      setError({
+        hasError: errorMessage,
+      })
+    );
   }
 
   setIsLoading && dispatch(setIsLoading({ isLoading: false }));
